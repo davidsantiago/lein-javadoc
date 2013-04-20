cf9270797d08468be822ff726bd4e6e0b0c07fa1
David Santiago <david.santiago@gmail.com>
2013-04-20 14:08:56 -0700

    Update version to 0.1.1 in project.clj.

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject lein-javadoc "0.1.1-SNAPSHOT"
+(defproject lein-javadoc "0.1.1"
   :description "Run javadoc for the java source in your lein project."
   :url "http://github.com/davidsantiago/lein-javadoc"
   :license {:name "Eclipse Public License"
